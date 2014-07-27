0ce7af6e0cf631ead82004a182be4307dc50f9b2
philipz <philipzheng@gmail.com>
2014-07-27 17:37:55 +0000

    First Init

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM phusion/baseimage:0.9.11
+MAINTAINER Philipz <philipzheng@gmail.com>
+
+RUN apt-get update
+RUN apt-get -y upgrade
+
+# Basic Requirements
+RUN apt-get -y install nginx php5-mysql php-apc curl unzip
+
+# Wordpress Requirements
+RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+
+# nginx config
+RUN sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
+RUN sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf
+RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+
+# HHVM install
+RUN curl http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
+RUN echo deb http://dl.hhvm.com/ubuntu trusty main | sudo tee /etc/apt/sources.list.d/hhvm.list
+RUN apt-get update && apt-get install -y hhvm
+
+# nginx site conf
+ADD ./nginx-site.conf /etc/nginx/sites-available/default
+
+# Install Wordpress
+ADD http://tw.wordpress.org/wordpress-3.9.1-zh_TW.tar.gz /usr/share/nginx/latest.tar.gz
+RUN cd /usr/share/nginx/ && tar xvf latest.tar.gz && rm latest.tar.gz
+RUN mv /usr/share/nginx/html/5* /usr/share/nginx/wordpress
+RUN rm -rf /usr/share/nginx/www
+RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
+ADD wp-config.php /usr/share/nginx/www/wp-config.php
+RUN chown -R www-data:www-data /usr/share/nginx/www
+
+# Download nginx helper plugin
+RUN curl -O `curl -i -s http://wordpress.org/plugins/nginx-helper/ | egrep -o "http://downloads.wordpress.org/plugin/[^']+"`
+RUN unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
+RUN chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
+
+RUN mkdir /etc/service/nginx
+ADD nginx.sh /etc/service/nginx/run
+
+RUN mkdir /etc/service/hhvm
+ADD hhvm.sh /etc/service/hhvm/run
+
+RUN sudo /usr/share/hhvm/install_fastcgi.sh
+
+# Define mountable directories.
+VOLUME ["/usr/share/nginx/www"]
+
+# private expose
+EXPOSE 80
+EXPOSE 443
+
+CMD ["/sbin/my_init"]
---
+++ wp-config.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+if( strlen(getenv("DB_NAME")) > 0 ) {
+  define('DB_NAME', getenv("DB_NAME"));
+} else {
+  define('DB_NAME', 'wordpress');
+}
+
+/** MySQL database username */
+if( strlen(getenv("DB_USER")) > 0 ) {
+  define('DB_USER', getenv("DB_USER"));
+} else {
+  define('DB_USER', 'admin');
+}
+
+/** MySQL database password */
+define('DB_PASSWORD', getenv("DB_PASSWORD"));
+
+/** MySQL hostname */
+if( strlen(getenv("DB_HOST")) > 0 ) {
+  define('DB_HOST', getenv("DB_HOST"));
+} else {
+  define('DB_HOST', (getenv("DB_1_PORT_3306_TCP_ADDR") . ":" . getenv("DB_1_PORT_3306_TCP_PORT")));
+}
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'roolaeth3cheuhooxagha6TheaHaidai9EeThaBeiZal2thie8aexohli0IQueiyu');
+define('SECURE_AUTH_KEY',  'eiwiujaejahlaima2faeta2shooz9phoo6LiaFam3phie9ho5iengie3oogee7roo');
+define('LOGGED_IN_KEY',    'Ru6Meih7iw3Sa1dux3EYaMah7thohnaebahghoh6ohlaitirahk3moTh5tihahsie');
+define('NONCE_KEY',        'phe8ew8eicethi9eiHash7ea1bu8nahch7zo6doo4ahngaeruaJoa8ohy6wah0Zae');
+define('AUTH_SALT',        'tiZ0Amae6chah2quee1thohHaeleex3neeZeje1uph9oobufei9noLee9Wo8heeth');
+define('SECURE_AUTH_SALT', 'aekooBae9quaivai7va4rai6zahyiechainaezum2Ighoag5aa2vee9phaiG7ohWu');
+define('LOGGED_IN_SALT',   'poquoveichahr4Uquijievae5Ulie8boh3Oke3eezo6ogh7zie6thahthaiphooDi');
+define('NONCE_SALT',       'eecaequeil0pei1egequai3Aghooluf9eighuj2Chaheo4uethaethooPh6Ohc0da');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', 'zh_TW');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+$plugins = get_option( 'active_plugins' );
+if ( count( $plugins ) === 0 ) {
+  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+  $wp_rewrite->set_permalink_structure( '/%postname%/' );
+  $pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+  foreach ( $pluginsToActivate as $plugin ) {
+    if ( !in_array( $plugin, $plugins ) ) {
+      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . $plugin );
+    }
+  }
+}
